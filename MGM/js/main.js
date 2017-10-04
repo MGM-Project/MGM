c5b4eb1ef29ba87372596ceaa1aaf232b151f047
MartinIvanov90 <martin.g.ivanov@gmail.com>
2017-10-05 00:09:44 +0300

    added second modal window, still no dropdown

--- MGM/js/main.js
+++ MGM/js/main.js
@@ -1,15 +1,30 @@
-﻿/* eslint-env jquery */
-$(window, document, undefined).ready(() => {
-  $('.login-modal-overlay').fadeIn(200);
-  $('.button').click(() => {
-    $('.login-modal-overlay').fadeOut(200);
-  });
-  $('.login-modal').click((event) => {
-    event.stopPropagation();
-  });
+﻿$(function() {
+                        // MODAL LOGIN START //
+    // Show overlay & Open modal
+    $(".login-modal-overlay").fadeIn(200);
+    $(".button").click(function() {
+      $(".login-modal-overlay").fadeOut(200);
+      $(".transparent-container").show();
+    });
+    //Attach event on .depositButton
+    $(".depositButton").click(function() {
+      $(".transparent-container").hide();
+    })
+    //Stops event execution of children elements
+    $(".login-modal").click(function(event) {
+      event.stopPropagation();
+    });
+    
+    
+    // Input label
+    
+    $('input').blur(function() {
+      var $this = $(this);
+      if ($this.val())
+        $this.addClass('used');
+      else
+        $this.removeClass('used');
+    });
 
-  $('input').blur(function () {
-    const $this = $(this);
-    if ($this.val()) { $this.addClass('used'); } else { $this.removeClass('used'); }
-  });
+                        // MODAL LOGIN END //
 });
