505738aa3ff64a0819669ed3eb1f92899aafd0f4
MartinIvanov90 <martin.g.ivanov@gmail.com>
2017-10-04 11:36:46 +0300

    added index.html, main.css, main.js, modal login

---
+++ Assets/js/main.js
@@ -0,0 +1,22 @@
+﻿$(window, document, undefined).ready(function() {
+
+    // Show overlay & Open modal
+    $(".login-modal-overlay").fadeIn(200);
+    $(".button").click(function() {
+      $(".login-modal-overlay").fadeOut(200);
+    })
+    $(".login-modal").click(function(event) {
+      event.stopPropagation();
+    });
+    
+    
+    // Input label
+  
+    $('input').blur(function() {
+      var $this = $(this);
+      if ($this.val())
+        $this.addClass('used');
+      else
+        $this.removeClass('used');
+    });
+});
