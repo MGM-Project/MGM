056677eb03d2933411ed4ed7e30f9fa9178acbad
Georgi Georgiev <georgiwess@gmail.com>
2017-10-04 14:19:42 +0300

    Fix file structure

--- MGM/js/main.js
+++ MGM/js/main.js
@@ -1,6 +1,5 @@
 ﻿$(window, document, undefined).ready(function() {
 
-    // Show overlay & Open modal
     $(".login-modal-overlay").fadeIn(200);
     $(".button").click(function() {
       $(".login-modal-overlay").fadeOut(200);
@@ -8,9 +7,6 @@
     $(".login-modal").click(function(event) {
       event.stopPropagation();
     });
-    
-    
-    // Input label
   
     $('input').blur(function() {
       var $this = $(this);
