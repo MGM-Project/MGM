b4b3ec899c858eadf5f099fce7d82108ce76fb03
Georgi Georgiev <georgiwess@gmail.com>
2017-10-07 22:33:29 +0300

    Small changes

---
+++ MGM/js/gameLogic.js
@@ -0,0 +1,7 @@
+// const asyncCardOps = asyncCardOpsFunc();
+// asyncCardOps.newShuffledDeck(5).then(deck => deck.deck_id)
+//   .then(deck => asyncCardOps.drawCard(deck))
+//   .then(picUrl => $('#logo').attr('src', picUrl.cards[0].image));
+
+// const balance = +$('#depositSum').val();
+// console.log(balance);
